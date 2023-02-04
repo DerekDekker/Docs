5387fa50b97150c9eb7bd2236ac73b0b094b79d1
DerekDekker <93021376+DerekDekker@users.noreply.github.com>
2023-02-04 23:24:03 +0800

    提交

--- 01.py
+++ 01.py
@@ -1,40 +1,71 @@
-class SingletonMeta(type):
-    """
-    The Singleton class can be implemented in different ways in Python. Some
-    possible methods include: base class, decorator, metaclass. We will use the
-    metaclass because it is best suited for this purpose.
-    """
+"""
+*What is this pattern about?
+The Borg pattern (also known as the Monostate pattern) is a way to
+implement singleton behavior, but instead of having only one instance
+of a class, there are multiple instances that share the same state. In
+other words, the focus is on sharing state instead of sharing instance
+identity.
 
-    _instances = {}
+*What does this example do?
+To understand the implementation of this pattern in Python, it is
+important to know that, in Python, instance attributes are stored in a
+attribute dictionary called __dict__. Usually, each instance will have
+its own dictionary, but the Borg pattern modifies this so that all
+instances have the same dictionary.
+In this example, the __shared_state attribute will be the dictionary
+shared between all instances, and this is ensured by assigning
+__shared_state to the __dict__ variable when initializing a new
+instance (i.e., in the __init__ method). Other attributes are usually
+added to the instance's attribute dictionary, but, since the attribute
+dictionary itself is shared (which is __shared_state), all other
+attributes will also be shared.
 
-    def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+*Where is the pattern used practically?
+Sharing state is useful in applications like managing database connections:
+https://github.com/onetwopunch/pythonDbTemplate/blob/master/database.py
+
+*References:
+- https://fkromer.github.io/python-pattern-references/design/#singleton
+- https://learning.oreilly.com/library/view/python-cookbook/0596001673/ch05s23.html
+- http://www.aleax.it/5ep.html
+
+*TL;DR
+Provides singleton-like behavior sharing state between instances.
+"""
+from typing import Dict
 
 
-class Singleton(metaclass=SingletonMeta):
-    def some_business_logic(self):
-        """
-        Finally, any singleton should define some business logic, which can be
-        executed on its instance.
-        """
+class Borg:
+    _shared_state: Dict[str, str] = {}
 
-        # ...
+    def __init__(self) -> None:
+        self.__dict__ = self._shared_state
+
+
+class YourBorg(Borg):
+    def __init__(self, state: str = None):
+        super().__init__()
+        if state:
+            self.state = state
+        else:
+            # initiate the first instance with default state
+            if not hasattr(self, "state"):
+                self.state = "Init"
+
+    def __str__(self) -> str:
+        return self.state
+
+
+def main():
+    rm1 = YourBorg()
+    rm2 = YourBorg()
+
+    rm1.state = 'Idle'
+    rm2.state = 'Running'
+
+    print('rm1: {0}'.format(rm1))
+    print('rm2: {0}'.format(rm2))
 
 
 if __name__ == "__main__":
-    # The client code.
-
-    s1 = Singleton()
-    s2 = Singleton()
-
-    if id(s1) == id(s2):
-        print("Singleton works, both variables contain the same instance.")
-    else:
-        print("Singleton failed, variables contain different instances.")
+    main()
--- docs/设计模式/单例模式_01/singleton.md
+++ docs/设计模式/单例模式_01/singleton.md
@@ -1,23 +1,33 @@
 # 单列模式
 
-继承 单列基类 的类多个实例都会是相同的实例
+
+- 让你能够保证一个类只有一个实例
+- 为该实例提供一个全局访问节点
+- 允许在程序的任何地方访问特定对象
+
+!!! note "" 
+
+    如果你创建了一个对象， 同时过一会儿后你决定再创建一个新对象， 此时你会获得之前已创建的对象， 而不是一个新对象。
 
 ---
 ## 单列基类
 
 ```python
-class Singleton:
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, '_instance'):
-            cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 ```
 
 ---
 ## 应用
 
 ```python
-class MyClass(Singleton):
+class MyClass(metaclass=SingletonMeta):
     def __init__(self, a):
         self.a = a
 ```
